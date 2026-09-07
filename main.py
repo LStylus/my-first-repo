6fabfffd8d17950e93834bcfc5eb330b99b3324c
LStylus <styluskarl.digdigan@wvsu.edu.ph>
2026-09-07 17:03:55 +0800

    Add subtract function

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ greet("World")
 
 
 def add(a,b):
-    """Subract two numbers and return the sum."""
+    """Add two numbers and return the sum."""
     return a+b
 
+def subtract(a,b):
+    """Subtract two numbers and return the difference."""
+    return a-b
+
 print(add(1,5))
+print(subtract(1,5))
 
 #stylus pogi
